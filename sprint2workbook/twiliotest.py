572eb57dd0eec7051be5501687834d8bff33fb12
Johanan Ottensooser <j@ottensooser.com>
2016-10-21 03:07:18 -0400

    Update twiliotest.py

--- sprint2workbook/twiliotest.py
+++ sprint2workbook/twiliotest.py
@@ -4,13 +4,19 @@ from twilio import twiml
 app = Flask(__name__)
 
 @app.route("/sms", methods=['GET', 'POST'])
-def sms_reply():
-    """Respond to incoming calls with a simple text message."""
+def incoming_sms():
+    """Send a dynamic reply to an incoming text message"""
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None)
+
     # Start our TwiML response
     resp = twiml.Response()
 
-    # Add a message
-    resp.message("The Robots are coming! Head for the hills!")
+    # Determine the right reply for this message
+    if body == 'hello':
+        resp.message("Hi!")
+    elif body == 'bye':
+        resp.message("Goodbye")
 
     return str(resp)
 
