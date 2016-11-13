4c5efca8bd0526d60d273b5ad5c12d271c942c0b
sindhu732 <sindhu732@gmail.com>
2016-11-13 03:56:14 -0500

    at v0.3

--- money_days_app/www/js/controllers.js
+++ money_days_app/www/js/controllers.js
@@ -1,29 +1,96 @@
 angular.module('app.controllers', [])
-  
-.controller('savePageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+
+.controller('savePageCtrl', ['$rootScope', '$scope', '$stateParams', '$http',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($rootScope, $scope, $stateParams, $http) {
 
+    $rootScope.moneydaysURL = 'http://10.128.19.187:8000';
+    $scope.totalSaved = 550;
+    $scope.goal = 2500;
+    $scope.progressToGoal = parseInt($scope.totalSaved/$scope.goal*100);
+
+    $scope.balanceInAccount = 400;
+    $rootScope.userID = 1;
+    $http({
+      method:'GET',
+      url: $scope.moneydaysURL + '/api/users/' + $scope.userID.toString,
+      headers : {
+        'Content-Type' : 'application/json',
+        'Authorization': 'Basic bXIyMjI0QGNvcm5lbGwuZWR1OnJvYmxlMTQwMTky'
+      }
+    }).then(function successCallback(response) {
+      console.log(response);
+    },
+    function errorCallback(response){
+      // offline alert - can't save money
+      console.log(response);
+    })
+
+    $scope.coachTips = [
+        "Post1.jpg", "Post2.jpg", "Post3.jpg"
+    ];
+
+    $rootScope.saveRecommended = 50;
+    $rootScope.saveAmount = 50;
+
+    $rootScope.save = function() {
+      if($scope.saveAmount < $scope.balanceInAccount){
+        console.log("Saved Amount", $scope.saveAmount);
+        $scope.totalSaved += $scope.saveAmount;
+        $scope.progressToGoal = parseInt($scope.totalSaved/$scope.goal*100);
+        $scope.balanceInAccount -= $scope.saveAmount;
+        // goaal reached ! - color red !, new badge
+        //
+        $http({
+          method: 'POST',
+          url: $scope.moneydaysURL + '/api/users/1/contributions/',
+          headers : {
+            'Content-Type' : 'application/json',
+            'Authorization': 'Basic bXIyMjI0QGNvcm5lbGwuZWR1OnJvYmxlMTQwMTky'
+          },
+          data: {txn_amount: $scope.saveAmount}
+        }).then(function successCallback(response) {
+            console.log("Saved successfully" + response);
+        }, function errorCallback(response) {
+            console.log("Error in saving" + response);
+        });
+      }
+      else
+        console.log("No balance in account");
+    };
+
+    $rootScope.less = function() {
+      if($scope.saveAmount>10)
+        $scope.saveAmount -= 10;
+    };
+
+    $rootScope.more = function() {
+      if($scope.saveAmount <= 0.4*$scope.balanceInAccount)
+        $scope.saveAmount += 10;
+      else
+        console.log("Greater than 40% of Balance in your Account");
+    };
 
 }])
-   
-.controller('lotteryPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+
+.controller('lotteryPageCtrl', ['$rootScope', '$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
+function ($rootScope, $scope, $stateParams) {
+  $scope.totalPoints = 250;
+  $scope.saveAmount = $rootScope.saveAmount;
 
 }])
-   
-.controller('socialPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+
+.controller('socialPageCtrl', ['$rootScope', '$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
+function ($rootScope, $scope, $stateParams) {
+  $scope.saveAmount = $rootScope.saveAmount;
 
 }])
-      
+
 .controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -31,7 +98,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('withdrawPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -39,7 +106,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('shopCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -47,7 +114,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('settingsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -55,4 +122,3 @@ function ($scope, $stateParams) {
 
 
 }])
- 
